73001ca1df06a65dd375bbb37537c9ee908ab18e
blessedsoy <unmigong@gmail.com>
2017-02-25 23:44:31 -0500

    Done.

---
+++ components/DumbComponent.js
@@ -0,0 +1,29 @@
+const React = require('react');
+
+class DumbComponent extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      mood: 'happy'
+    }
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(){
+    this.state.mood === 'happy' ? this.setState({mood: 'sad'}) : this.setState({mood: 'happy'})
+  }
+
+  render(){
+    return(
+      <div onClick={this.handleClick}> 
+        {this.state.mood}
+      </div>
+    )
+    
+  }
+
+}
+
+module.exports = DumbComponent;
+
